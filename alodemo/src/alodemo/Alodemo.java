a81c5833888755ffbc06bfec619a162dc37328d9
marshallein <72290795+marshallein@users.noreply.github.com>
2021-05-12 12:00:37 +0700

    Update Alodemo.java

--- alodemo/src/alodemo/Alodemo.java
+++ alodemo/src/alodemo/Alodemo.java
@@ -17,6 +17,7 @@ public class Alodemo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("heeeloo betch");
+        System.out.println("heeeloo betch mn2");
     }
     
 }
